bba938f8fd13ce5931bad8cfd69e2d87ef236cb3
Gideon Farrell <me@gideonfarrell.co.uk>
2014-02-25 01:29:31 +0000

    create entry point for javascript app

---
+++ mayballmarketplace/mayballmarketplace/static/js/main.js
@@ -0,0 +1,10 @@
+/* global require */
+require.config({
+	paths: {
+		jquery: '../lib/jquery/dist/jquery',
+		bootstrap: '../lib/bootstrap/js'
+	}
+});
+require([/* requirements for entry point */], function(/* ... */) {
+	// Main entry point
+});
