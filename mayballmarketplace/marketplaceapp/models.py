f71b4b059985ba19bfe4cd198f37f7dd8efa4deb
Neil Satra <github.k6p@gishpuppy.com>
2014-03-01 00:24:42 +0000

    Add basic models for event and ticket class, with tests

--- mayballmarketplace/marketplaceapp/models.py
+++ mayballmarketplace/marketplaceapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    start_date = models.DateField()
+    # cover_image = models.ImageField()
+
+    @property
+    def is_not_in_past(self):
+        return self.start_date >= date.today()
+
+    def __str__(self):
+        return self.name
+
+class TicketClass(models.Model):
+    event = models.ForeignKey(Event)
+    name = models.CharField(max_length=255)
+    base_price = models.DecimalField(max_digits = 6, decimal_places = 2)
+
+    def __str__(self):
+        return self.name
--- mayballmarketplace/marketplaceapp/tests.py
+++ mayballmarketplace/marketplaceapp/tests.py
@@ -6,11 +6,20 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from marketplaceapp.models import *
+from datetime import date, timedelta
 
+class EventTests(TestCase):
+    def test_event_tommorow(self):
+        tomorrow = date.today() + timedelta(days=1)
+        event = Event(name="Pembroke Test Event", start_date=tomorrow)
+        self.assertTrue(event.is_not_in_past)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_event_today(self):
+        event = Event(name="Pembroke Test Event", start_date=date.today())
+        self.assertTrue(event.is_not_in_past)
+
+    def test_event_yesterday(self):
+        yesterday = date.today() - timedelta(days=1)
+        event = Event(name="Pembroke Test Event", start_date=yesterday)
+        self.assertFalse(event.is_not_in_past)
