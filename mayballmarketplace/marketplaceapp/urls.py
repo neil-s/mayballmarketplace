395fc76c39ae090cac937dc4e5609d73af5e2960
Neil Satra <github.k6p@gishpuppy.com>
2014-03-02 18:38:14 +0000

    Switch to Django's static files system: - Use the static file helper in templates rather than hard-coded URLs - Fix settings so Django knows where to find templates and statics - Fix URLs so static files can be served - Change index view to actually load index template, so it can be developed on easily.

--- mayballmarketplace/marketplaceapp/urls.py
+++ mayballmarketplace/marketplaceapp/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from marketplaceapp import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index')
 )
+
+urlpatterns += staticfiles_urlpatterns()
--- mayballmarketplace/mayballmarketplace/settings.py
+++ mayballmarketplace/mayballmarketplace/settings.py
@@ -60,17 +60,20 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.dirname(os.path.realpath(__file__)) + '../static'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
+
+BASE_DIR = os.path.abspath(os.path.dirname(__name__))
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, "static"),
 )
 
 # List of finder classes that know how to find static files in
@@ -110,6 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, "templates"),
 )
 
 INSTALLED_APPS = (
