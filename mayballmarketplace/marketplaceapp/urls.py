1d3d0959aba05fa9a4c4ac951112fecef3bd3e18
Neil Satra <github.k6p@gishpuppy.com>
2014-03-18 23:40:39 +0530

    Add view for the event page, which shows all buy and sell offers. Currently, the URL scheme includes both the Event ID and an optional slug of the event name ('johns-test-event-2014'). The event ID is used to actually load the event, the slug  is completely ignored currently and is just there for humans to have pretty URLs.

--- mayballmarketplace/marketplaceapp/urls.py
+++ mayballmarketplace/marketplaceapp/urls.py
@@ -4,7 +4,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from marketplaceapp import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+                       #url(r'^$', views.index, name='index'),
+                       url(r'^event/(?P<event_id>\d+)/$', view=views.event_offers, name='Event Offers'),
+                       url(r'^event/(?P<event_id>\d+)/(?P<slug>[-\w\d]+)/$', view=views.event_offers,
+                           name='Event Offers'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
--- mayballmarketplace/marketplaceapp/views.py
+++ mayballmarketplace/marketplaceapp/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import *
+from marketplaceapp.models import *
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def event_offers(request, event_id="1", slug=""):
+    event = get_object_or_404(Event, id=event_id)
+    offers = Offer.objects.filter(ticket_class__event=event)
+    buy_offers = offers.filter(type='B')
+    sell_offers = offers.filter(type='S')
+    return render(request, 'event.html', {"buy_offers": buy_offers, "sell_offers": sell_offers})
