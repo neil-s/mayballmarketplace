a537afeb5c431e8210f8ba95f4d10a0cc83d6326
Neil Satra <github.k6p@gishpuppy.com>
2014-03-18 09:28:39 +0530

    Expose Django's admin panel to easily create mock objects.

---
+++ mayballmarketplace/marketplaceapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from marketplaceapp.models import Event, TicketClass, Offer
+
+admin.site.register(Event)
+admin.site.register(TicketClass)
+admin.site.register(Offer)
