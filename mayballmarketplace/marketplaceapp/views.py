7fa2d53092cb5cb2027cc637bcf6401573816dc1
Neil Satra <github.k6p@gishpuppy.com>
2014-03-20 06:15:18 +0530

    Add home page that shows a listing of all future events.

--- mayballmarketplace/marketplaceapp/views.py
+++ mayballmarketplace/marketplaceapp/views.py
@@ -12,3 +12,8 @@ def event_offers(request, event_id="1", slug=""):
     buy_offers = offers.filter(type='B')
     sell_offers = offers.filter(type='S')
     return render(request, 'event.html', {"buy_offers": buy_offers, "sell_offers": sell_offers})
+
+
+def event_list(request):
+    events = get_list_or_404(Event, start_date__gte=date.today())  # start_date >= date.today()
+    return render(request, 'event_list.html', {'events': events})
